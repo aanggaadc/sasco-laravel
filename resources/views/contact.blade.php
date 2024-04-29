45127403374c6aa854d320962809bca03068c1c5
anggara.setiawan <aanggaadc@gmail.com>
2024-04-30 00:50:47 +0700

    restyling notification success send email

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,6 +6,39 @@
     semua kebutuhan energi Anda.')
 
 @section('content')
+    @if (session()->has('success'))
+        <div class="fixed left-0 top-[50px] z-[1] w-full md:top-[120px]">
+            <div
+                class="relative mx-auto mb-3 mt-3 flex max-w-[500px] flex-col rounded-md bg-gray-200 py-5 pl-6 pr-8 shadow dark:bg-green-700 sm:flex-row sm:items-center sm:pr-6">
+                <div class="flex w-full flex-row items-center border-b pb-4 sm:w-auto sm:border-b-0 sm:pb-0">
+                    <div class="text-green-500" dark:text-gray-500>
+                        <svg class="h-6 w-6 sm:h-5 sm:w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                    </div>
+                    <div class="ml-3 text-sm font-medium">Success!</div>
+                </div>
+                <div class="mt-4 text-sm tracking-wide text-gray-500 dark:text-white sm:ml-4 sm:mt-0">
+                    {{ session('success') }}
+                </div>
+                <div
+                    class="absolute right-4 top-4 ml-auto cursor-pointer text-gray-400 hover:text-gray-800 sm:relative sm:right-auto sm:top-auto">
+                    <button
+                        class="absolute right-4 top-4 ml-auto cursor-pointer text-gray-400 hover:text-gray-800 sm:relative sm:right-auto sm:top-auto"
+                        onclick="this.parentElement.parentElement.remove()">
+                        <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                            </path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    @endif
+
     <section
         class="flex h-[calc(100vh_-_56px)] w-full flex-col justify-center bg-[url('/public/images/bg-hero-kontak-mobile.jpg')] bg-cover bg-center bg-no-repeat px-[35px] md:h-[calc(100vh_-_123px)] md:bg-[url('/public/images/bg-hero-kontak-desktop.jpg')] md:bg-[bottom_right] lg:px-[10%]">
         <h2
@@ -40,37 +73,6 @@
                     class="mb-[18px] text-center text-[28px] font-bold leading-[42px] text-[#383838] lg:mb-[39px] lg:text-left lg:text-4xl lg:leading-[54px]">
                     Tanya Kami
                 </h2>
-
-                @if (session()->has('success'))
-                    <div
-                        class="relative mb-3 mt-3 flex flex-col rounded-md bg-gray-200 py-5 pl-6 pr-8 shadow dark:bg-green-700 sm:flex-row sm:items-center sm:pr-6">
-                        <div class="flex w-full flex-row items-center border-b pb-4 sm:w-auto sm:border-b-0 sm:pb-0">
-                            <div class="text-green-500" dark:text-gray-500>
-                                <svg class="h-6 w-6 sm:h-5 sm:w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                </svg>
-                            </div>
-                            <div class="ml-3 text-sm font-medium">Success!</div>
-                        </div>
-                        <div class="mt-4 text-sm tracking-wide text-gray-500 dark:text-white sm:ml-4 sm:mt-0">
-                            {{ session('success') }}
-                        </div>
-                        <div
-                            class="absolute right-4 top-4 ml-auto cursor-pointer text-gray-400 hover:text-gray-800 sm:relative sm:right-auto sm:top-auto">
-                            <button
-                                class="absolute right-4 top-4 ml-auto cursor-pointer text-gray-400 hover:text-gray-800 sm:relative sm:right-auto sm:top-auto"
-                                onclick="this.parentElement.parentElement.remove()">
-                                <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"></path>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                @endif
 
                 <form action="{{ route('contact.send') }}" method="POST" class="font-poppins">
                     @csrf
