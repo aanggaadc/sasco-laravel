1c3316fc0ff05310cbd02217c5819fc9c222c2e3
anggara.setiawan <aanggaadc@gmail.com>
2024-05-02 16:04:27 +0700

    add canonical tag

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,6 +12,13 @@
 
     {{-- Alphine.JS --}}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    {{-- Canonical Tag --}}
+    {!! '<link rel="canonical" href="https://sascopetroniaga.id' .
+        (Request::path() !== '/' ? '/' : '') .
+        Request::path() .
+        '"/>' !!}
+
 </head>
 
 <body>
