a474b7adf69af317922c8b8b69f74ed144e42578
anggara.setiawan <aanggaadc@gmail.com>
2024-05-07 10:12:43 +0700

    add og and twitter meta

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,11 +3,23 @@
 
 <head>
     <title>@yield('title', 'Sasco Petroniaga - Penyedia Bahan Bakar Industri Terpercaya')</title>
-    <meta name="description" content="@yield('description', 'Sejak 2016, Sasco Petroniaga menjadi penyedia terpercaya bahan bakar minyak industri (HSD/solar) di Kepulauan Riau, menawarkan layanan tepat waktu dengan kualitas dan harga kompetitif.')">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="@yield('title', 'Sasco Petroniaga - Penyedia Bahan Bakar Industri Terpercaya')">
+    <meta property="og:title" content="@yield('title', 'Sasco Petroniaga - Penyedia Bahan Bakar Industri Terpercaya')">
+    <meta name="description" content="@yield('description', 'Sejak 2016, Sasco Petroniaga menjadi penyedia terpercaya bahan bakar minyak industri (HSD/solar) di Kepulauan Riau, menawarkan layanan tepat waktu dengan kualitas dan harga kompetitif.')">
+    <meta property="og:description" content="@yield('description', 'Sejak 2016, Sasco Petroniaga menjadi penyedia terpercaya bahan bakar minyak industri (HSD/solar) di Kepulauan Riau, menawarkan layanan tepat waktu dengan kualitas dan harga kompetitif.')">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="sascopetroniaga.id/favicon.ico">
+    <meta property="og:image:width" content="58">
+    <meta property="og:image:height" content="112">
     <meta name="keywords"
         content="energy, oil, gas, diesel, solar, fuel, MGO, pertadex, dexlite, supplier, distributor, industry, marine, batam,  Kepulauan Riau, Indonesia">
+    <meta name="twitter:creator" content="Sasco Petroniaga">
+    <meta name="twitter:title" content="@yield('title', 'Sasco Petroniaga - Penyedia Bahan Bakar Industri Terpercaya')">
+    <meta name="twitter:description" content="@yield('description', 'Sejak 2016, Sasco Petroniaga menjadi penyedia terpercaya bahan bakar minyak industri (HSD/solar) di Kepulauan Riau, menawarkan layanan tepat waktu dengan kualitas dan harga kompetitif.')">
+
+    <meta name="robots" content="{{ env('APP_ENV') === 'production' ? 'index, follow' : 'noindex, nofollow' }}">
     @vite('resources/css/app.css')
 
     {{-- Alphine.JS --}}
